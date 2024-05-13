50767b4c65e46a24e6e5c73d76cc1e9206288061
doaasalama23 <doaasalamaadam@gmail.com>
2024-05-13 13:57:03 +0300

    login-ui & integration

---
+++ src/Context/AuthContext.tsx
@@ -0,0 +1,61 @@
+
+import  { jwtDecode} from "jwt-decode";
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+// Define the type of the decoded token
+interface DecodedToken {
+  userGroup: string;
+  // Add other properties if needed
+}
+interface AuthContextType {
+  saveAdminData: () => void;
+  // adminData?: string | null ,
+  adminData :DecodedToken | null 
+  
+  userRole :string | null
+  Token? : string | null
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [adminData, setAdminData] = useState<DecodedToken | null>(null);
+  const [userRole, setUserRole] = useState<string | null>(null);
+  console.log(userRole)
+ const Token = localStorage.getItem("adminToken") ;
+ 
+  const saveAdminData = () => {
+    const encodedToken = localStorage.getItem("adminToken");
+    if (encodedToken) {
+      const decodedToken = jwtDecode(encodedToken) as DecodedToken;
+      setAdminData(decodedToken);
+      setUserRole(decodedToken?.userGroup)
+    }
+  };
+
+  useEffect(() => {
+    saveAdminData();
+  }, []);
+
+  const contextValue: AuthContextType = {
+    adminData,
+    userRole,
+    saveAdminData,
+    Token
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
+// Custom hook to use the toast context
+export const useUser = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+  return context;
+};
--- src/Modules/SharedModule/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Modules/SharedModule/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,19 @@
-import React from "react";
 
-export default function ProtectedRoute() {
-  return <>ProtectedRoute</>;
-}
+import { ReactNode} from 'react';
+import { Navigate } from 'react-router-dom';
+import { useUser } from '../../../../Context/AuthContext';
+
+
+const ProtectedRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
+
+  const {adminData} = useUser();
+  
+  if (adminData == null && localStorage.getItem("adminToken") == null) {
+    return <Navigate to="/login" />;
+  } else {
+    return children; // Return the children directly
+  }
+};
+
+export default ProtectedRoute;
+

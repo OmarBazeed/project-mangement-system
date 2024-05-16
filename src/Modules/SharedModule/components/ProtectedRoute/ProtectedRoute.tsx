d625302b70f3c9da7170773f648bf25f9242d2dc
Omar Bazeed <114960595+OmarBazeed@users.noreply.github.com>
2024-05-16 23:47:12 +0300

    Fixing Bugs On Protected Route

--- src/Modules/SharedModule/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Modules/SharedModule/components/ProtectedRoute/ProtectedRoute.tsx
@@ -5,7 +5,7 @@ import { useUser } from "../../../../Context/AuthContext";
 const ProtectedRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { adminData } = useUser();
 
-  if (adminData == null || !localStorage.getItem("token")) {
+  if (adminData == null && !localStorage.getItem("token")) {
     return <Navigate to="/login" />;
   } else {
     return children; // Return the children directly
